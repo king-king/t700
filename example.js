2c6233163ea546981933384339bbdf406a0d6a98
WQ <niuyiwangqun@sohu.com>
2015-05-19 16:00:39 +0800

    c

--- example.js
+++ example.js
@@ -38,14 +38,60 @@ main( function () {
         };
     }
 
+    function requestAnimate( duration, arg ) {
+        var s = (new Date()).getTime();
+
+        function animate() {
+            var curTime = (new Date()).getTime();
+            if ( curTime - s < duration ) {
+                arg.onAnimate( {
+                    progress : (curTime - s) / duration
+                } );
+                setTimeout( function () {
+                    animate();
+                }, 1000 / 60 );
+            }
+            else {
+                // ½áÊø
+                arg.onEnd();
+            }
+        }
+
+        animate();
+    }
+
     var page = document.querySelector( ".page" );
     page.y = 0;
-    point.bind( container, "touchstart", function () {
+
+    var isTouching = false;
+
+    point.bind( container, "touchstart", function ( e ) {
+        e.preventDefault();
+        if ( isTouching ) {
+            return;
+        }
+        isTouching = true;
         onMoveUp( {
             onMove : function ( e ) {
                 css( page, {
-                    transform : "translate3d(0," + (page.y += e.dY) + "px,0)"
+                    "-webkit-transform" : "translate3d(0," + (page.y += e.dY) + "px,0)"
                 } )
+            },
+            onUp : function () {
+                requestAnimate( 300, {
+                    onAnimate : function ( arg ) {
+                        css( page, {
+                            "-webkit-transform" : "translate3d(0," + (page.y - page.y * arg.progress) + "px,0)"
+                        } );
+                    },
+                    onEnd : function () {
+                        css( page, {
+                            "-webkit-transform" : "translate3d(0,0,0)"
+                        } );
+                        page.y = 0;
+                        isTouching = false;
+                    }
+                } );
             }
         } );
 
