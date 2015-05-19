f021ba21bf890d4960eb6ff0b43a27df97bacb43
WQ <niuyiwangqun@sohu.com>
2015-05-19 15:13:28 +0800

    c

--- example.js
+++ example.js
@@ -5,9 +5,51 @@ main( function () {
 
     var point = imports( "pointer" );
 
+    var container = document.querySelector( ".container" );
+
+    function loopObj( obj, func ) {
+        for ( var key in obj ) {
+            func( key, obj[key] );
+        }
+    }
+
+    function css( el, style ) {
+        loopObj( style, function ( key, value ) {
+            el.style.setProperty( key, value, null );
+        } );
+    }
+
+    function onMoveUp( arg ) {
+        var moveHandle = point.onPointerMove( document, function ( event ) {
+            arg.onMove && arg.onMove( event );
+        } );
+
+        var upHandle = point.onPointerUp( document, function ( event ) {
+            moveHandle.remove();
+            upHandle.remove();
+            arg.onUp && arg.onUp( event );
+        } );
+
+        return {
+            remove : function () {
+                moveHandle.remove();
+                upHandle.remove();
+            }
+        };
+    }
+
+    var page = document.querySelector( ".page" );
+    page.y = 0;
+    point.bind( container, "touchstart", function () {
+        onMoveUp( {
+            onMove : function ( e ) {
+                css( page, {
+                    transform : "translate3d(0," + (page.y += e.dY) + "px,0)"
+                } )
+            }
+        } );
+
+    } )
 
 
-
-
-    
 } );
